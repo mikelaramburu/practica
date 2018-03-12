72e531f7d2c4808fc19398bbe4fab6c2be49d2f9
Curso <Curso@IPARTEKAULA.COM>
2018-03-12 14:31:27 +0100

    MTX12_tareas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient, HttpHandler } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { UnoComponent } from './uno/uno.component';
+import { ServicioService } from './providers/servicio.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ServicioService,HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
